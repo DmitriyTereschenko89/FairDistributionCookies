06ad77da55475ef5a7ae71746ce65f4433b81a16
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-01 15:24:15 +0300

    Добавьте файлы проекта.

---
+++ FairDistributionCookies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairDistributionCookies
+{
+	internal class FairDistributionCookies
+	{
+		private int DFS(int[] distributions, int[] cookies, int i, int k, int zeroCount)
+		{
+
+		}
+
+		public int DistributeCookies(int[] cookies, int k)
+		{
+			int[] distributions = new int[k];
+		}
+	}
+}
