ee95973f272febca39186d4e5d656003d1e1f6e8
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-01 15:36:06 +0300

    update read.me, add solution

--- FairDistributionCookies.cs
+++ FairDistributionCookies.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FairDistributionCookies
+﻿namespace FairDistributionCookies
 {
 	internal class FairDistributionCookies
 	{
 		private int DFS(int[] distributions, int[] cookies, int i, int k, int zeroCount)
 		{
-
+			if (cookies.Length - i < zeroCount)
+			{
+				return int.MaxValue;
+			}
+			if (i == cookies.Length)
+			{
+				int unfairless = int.MinValue;
+				foreach(int distribution in distributions)
+				{
+					unfairless = Math.Max(unfairless, distribution);
+				}
+				return unfairless;
+			}
+			int answer = int.MaxValue;
+			for (int j = 0; j < k; ++j)
+			{
+				zeroCount -= distributions[j] == 0 ? 1 : 0;
+				distributions[j] += cookies[i];
+				answer = Math.Min(answer, DFS(distributions, cookies, i + 1, k, zeroCount));
+				distributions[j] -= cookies[i];
+				zeroCount += distributions[j] == 0 ? 1 : 0;
+			}
+			return answer;
 		}
 
 		public int DistributeCookies(int[] cookies, int k)
 		{
 			int[] distributions = new int[k];
+			return DFS(distributions, cookies, 0, k, k);
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			FairDistributionCookies fairDistributionCookies = new();
+            Console.WriteLine(fairDistributionCookies.DistributeCookies(new int[] { 8, 15, 10, 20, 8 }, 2));
+			Console.WriteLine(fairDistributionCookies.DistributeCookies(new int[] { 6, 1, 3, 2, 2, 4, 1, 2 }, 3));
 		}
 	}
 }
